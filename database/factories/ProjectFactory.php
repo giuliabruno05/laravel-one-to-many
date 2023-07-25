bed152b01573a1866aab505ba4703e7a23a114f2
giuliabruno05 <giuliabruno48@gmail.com>
2023-07-25 12:50:41 +0200

    first commit

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake() -> words (3,true),
+            'description' => fake() -> paragraph (),
+            'start_date' => fake() -> date(),
+            'end_date' => fake() -> date(),
+            'img' => "https://picsum.photos/400/200"
+        ];
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container text-center mt-5">
+    <h4 class="p-2 mb-2  text-white">PROJECT</h4>
+    <ul>
+        @foreach ($projects as $project)
+            <li>
+                @auth
+                <a href="{{route('project.show', $project -> id)}}"class="link-opacity-50">
+                    {{$project -> name}}
+                </a>
+                @endauth
+                @guest
+                    {{$project -> name}}
+                @endguest
+            </li>
+        @endforeach
+    </ul>
+</div>
+@endsection
