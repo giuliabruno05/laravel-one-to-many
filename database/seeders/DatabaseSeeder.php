27199ba42a7a2d6bb5a191241ba7ba27f1fad98d
giuliabruno05 <giuliabruno48@gmail.com>
2023-07-25 21:44:33 +0200

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this -> call([
-            ProjectTableSeeder :: class
+            ProjectTableSeeder :: class,
+            TypeTableSeeder :: class
         ]);
     }
 }
---
+++ database/seeders/TypeTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use App\Models\Type;
+
+
+
+class TypeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+       $types = Type :: factory() -> count(12) -> make();
+
+       foreach ($types as $type) {
+            $project = Project :: inRandomOrder() -> first();
+
+            $type -> project_id = $project -> id;
+            $type -> save();
+       }
+    }
+}
